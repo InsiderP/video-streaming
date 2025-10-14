911eec7e7a6e2c87e6c78ed3d8fa2ea730628ec2
Pragati Singh <pragatisinghshreya8303@gmail.com>
2025-10-14 21:04:51 +0530

    commit

--- frontend/src/components/video/VideoPlayer.tsx
+++ frontend/src/components/video/VideoPlayer.tsx
@@ -566,7 +566,7 @@ const VideoPlayer: React.FC = () => {
               </div>
               <div className="flex justify-between">
                 <span>H</span>
-                <span className="text-gray-300">Shows/Hidennnnn Helpss</span>
+                <span className="text-gray-300">Shows/Hidennnn Helpss</span>
               </div>
             </div>
           </div>
